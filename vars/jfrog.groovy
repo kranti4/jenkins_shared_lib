fa80213303f0d5869f5e62e10a9597c4eec378c9
kranti4 <146917885+kranti4@users.noreply.github.com>
2023-11-25 14:36:01 +0530

    Update jfrog.groovy

--- vars/jfrog.groovy
+++ vars/jfrog.groovy
@@ -1,4 +1,3 @@
 def call(){
-    sh 'docker start artifactory'
     sh 'curl -X PUT -u admin:password -T /var/lib/jenkins/workspace/java/target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar http://52.91.115.150:8082/artifactory/example-repo-local/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar'
 }
